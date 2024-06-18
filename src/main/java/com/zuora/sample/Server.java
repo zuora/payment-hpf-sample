62044f530a7f63f7f3d591f3ae57aed34888fa15
Feng Xi <fxi@zuora.com>
2024-06-18 11:14:25 +0800

    PAY-22362: use new zuora.js in sample code.

--- src/main/java/com/zuora/sample/Server.java
+++ src/main/java/com/zuora/sample/Server.java
@@ -6,13 +6,11 @@ import static spark.Spark.staticFiles;
 
 import com.zuora.ZuoraClient;
 import com.zuora.api.ObjectQueriesApi;
-import com.zuora.model.CreateAccountContact;
-import com.zuora.model.CreateAccountRequest;
-import com.zuora.model.CreateAccountResponse;
-import com.zuora.model.ExpandedAccount;
+import com.zuora.model.*;
 
 import com.google.gson.Gson;
 
+import java.math.BigDecimal;
 import java.nio.file.Paths;
 import java.util.Map;
 
@@ -30,26 +28,18 @@ public class Server {
         zuoraClient.initialize();
         zuoraClient.setDebugging(true);
 
-        post("/get-billing-account", (request, response) -> {
+        post("create-payment-session", (request, response) -> {
             response.type("application/json");
             Map map = gson.fromJson(request.body(), Map.class);
-            String accountName = (String) map.get("name");
-            if (accountName != null) {
-                ObjectQueriesApi objectQueriesApi = zuoraClient.objectQueriesApi();
-                ExpandedAccount account = objectQueriesApi.queryAccountByKeyApi(accountName).execute();
-                if (account != null) {
-                    return gson.toJson(account.getId());
-                }
-            }
-
             String firstName = (String) map.get("firstName");
             String lastName = (String) map.get("lastName");
             String country = (String) map.get("country");
             String currency = (String) map.get("currency");
+            Integer amount = (Integer) map.get("amount");
+
             final CreateAccountContact contact = new CreateAccountContact().firstName(firstName)
                     .lastName(lastName)
                     .country(country);
-
             final CreateAccountRequest createAccountRequest = new CreateAccountRequest()
                     .name(String.join(" ", firstName, lastName))
                     .billToContact(contact)
@@ -57,10 +47,16 @@ public class Server {
                     .soldToSameAsBillTo(true) // alternatively, use .soldToContact(contact)
                     .autoPay(false)
                     .currency(currency);
-
             final CreateAccountResponse createAccountResponse = zuoraClient.accountsApi().createAccountApi(createAccountRequest).execute();
-            return gson.toJson(createAccountResponse.getAccountId());
 
+            final CreatePaymentSessionRequest createPaymentSessionRequest = new CreatePaymentSessionRequest()
+                    .currency(currency)
+                    .amount(new BigDecimal(amount))
+                    .processPayment(true)
+                    .accountId(createAccountResponse.getAccountId());
+            final CreatePaymentSessionResponse createPaymentSessionResponse = zuoraClient.paymentMethodsApi().createPaymentSessionApi(createPaymentSessionRequest).execute();
+
+            return gson.toJson(createPaymentSessionResponse.getToken());
         });
     }
 }
